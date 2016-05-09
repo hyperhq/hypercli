4549d68fc2e75fd3ca4cadfc95047bb5c8837349
Xianglin Gao <xlgao@zju.edu.cn>
2016-05-09 13:25:59 +0800

    add TestAssociateConfedContainer
    Signed-off-by: Xianglin Gao <xlgao@zju.edu.cn>

--- integration-cli/docker_cli_fip_test.go
+++ integration-cli/docker_cli_fip_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-check/check"
 )
 
+//TODO: release a non-IP
 func (s *DockerSuite) TestAssociateUsedIP(c *check.C) {
 	printTestCaseName()
 	defer printTestDuration(time.Now())
@@ -20,16 +21,41 @@ func (s *DockerSuite) TestAssociateUsedIP(c *check.C) {
 	defer releaseFip(c, fipList)
 
 	out, _ = runSleepingContainer(c, "-d")
-	firstID := strings.TrimSpace(out)
-	fmt.Println(firstID)
+	firstContainerID := strings.TrimSpace(out)
+	fmt.Println(firstContainerID)
 
 	out, _ = runSleepingContainer(c, "-d")
-	secondID := strings.TrimSpace(out)
-	fmt.Println(secondID)
+	secondContainerID := strings.TrimSpace(out)
+	fmt.Println(secondContainerID)
 
-	dockerCmd(c, "fip", "associate", firstIP, firstID)
-	out, _, err := dockerCmdWithError("fip", "associate", firstIP, secondID)
+	dockerCmd(c, "fip", "associate", firstIP, firstContainerID)
+	out, _, err := dockerCmdWithError("fip", "associate", firstIP, secondContainerID)
 	c.Assert(err, checker.NotNil, check.Commentf("Should fail.", out, err))
-	out, _ = dockerCmd(c, "fip", "disassociate", firstID)
+	out, _ = dockerCmd(c, "fip", "disassociate", firstContainerID)
+	c.Assert(out, checker.Equals, firstIP+"\n")
+}
+
+func (s *DockerSuite) TestAssociateConfedContainer(c *check.C) {
+	printTestCaseName()
+	defer printTestDuration(time.Now())
+
+	out, _ := dockerCmd(c, "fip", "allocate", "1")
+	firstIP := strings.TrimSpace(out)
+	fmt.Println(firstIP)
+	fipList := []string{firstIP}
+
+	out, _ = dockerCmd(c, "fip", "allocate", "1")
+	secondIP := strings.TrimSpace(out)
+	fipList = append(fipList, secondIP)
+	defer releaseFip(c, fipList)
+
+	out, _ = runSleepingContainer(c, "-d")
+	firstContainerID := strings.TrimSpace(out)
+	fmt.Println(firstContainerID)
+
+	dockerCmd(c, "fip", "associate", firstIP, firstContainerID)
+	out, _, err := dockerCmdWithError("fip", "associate", secondIP, firstContainerID)
+	c.Assert(err, checker.NotNil, check.Commentf("Should fail.", out, err))
+	out, _ = dockerCmd(c, "fip", "disassociate", firstContainerID)
 	c.Assert(out, checker.Equals, firstIP+"\n")
 }
